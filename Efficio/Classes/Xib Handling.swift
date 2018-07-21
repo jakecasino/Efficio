b6f272d3056db0aa49c8c2d56e7b88beaf4df89e
Jake Casino <jake@jakecasino.com>
2018-07-20 22:26:59 -0700

    Added Xib handling

---
+++ Efficio/Classes/Xib Handling.swift
@@ -0,0 +1,15 @@
+//
+//  Xib Handling.swift
+//  Efficio
+//
+//  Created by Jake Casino on 7/20/18.
+//
+
+import UIKit
+
+extension UIView {
+	public func loadXib(named name: String, view: UIView) {
+		Bundle.main.loadNibNamed(name, owner: self, options: nil)
+		addSubview(view)
+	}
+}
